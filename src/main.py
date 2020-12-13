1beaffd9c95d16c90defd11717a3863954faeca0
matti16 <bmattia92@gmail.com>
2020-12-13 12:32:27 +0100

    refactor main

--- src/main.py
+++ src/main.py
@@ -6,21 +6,43 @@ from raspi_clock.controllers import Alarm
 
 
 
-def main():
-    alarm_ctrl = Alarm()
-
-    x = threading.Thread(target=alarm_ctrl.show_current_time)
-    x.start()
+class RaspiClock():
     
-    alarms = alarm_ctrl.read_alarms()
+    def __init__(self):
+        self.alarm = Alarm()
+        self.alarms = self.alarm.read_alarms()
 
-    for a in alarms:
-        print(f"Scheduling alarm at {a}")
-        schedule.every().day.at(a).do(alarm_ctrl.start_alarm)
 
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+    def start_display_time_thread(self):
+        self.display_thread = threading.Thread(target=self.alarm.show_current_time)
+        self.display_thread.start()
+
+
+    def start_alarm(self):
+        self.display_thread._stop()
+        self.alarm.start_alarm()
+        self.start_display_time_thread()
+
+
+    def schedule_alarms(self):
+        schedule.clear()
+        for a in self.alarms:
+            print(f"Scheduling alarm at {a}")
+            schedule.every().day.at(a).do(self.start_alarm)
+
+
+    def schedule_loop(self):
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+
+
+
+def main():
+    main_alarm = RaspiClock()
+    main_alarm.start_display_time_thread()
+    main_alarm.schedule_alarms()
+    main_alarm.schedule_loop()
 
 
 if __name__ == "__main__":

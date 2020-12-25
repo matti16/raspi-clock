bcfd76d7837e47168721569e2c0ab4d8187fb64e
matti16 <bmattia92@gmail.com>
2020-12-25 20:15:03 +0100

    auto connect

--- auto_connect.sh
+++ auto_connect.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-sudo -u pi pulseaudio --start << EOF
+pulseaudio --start << EOF
 raspberry
 EOF
 
-sudo -u pi bluetoothctl << EOF
+bluetoothctl << EOF
 raspberry
 connect F4:4E:FD:3A:9F:87
 EOF

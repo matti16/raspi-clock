4e05c8a1082193a36bd23fd219eb4157164714c6
matti16 <bmattia92@gmail.com>
2020-12-25 19:07:53 +0100

    programming joystick

--- src/raspi_clock/controllers/__init__.py
+++ src/raspi_clock/controllers/__init__.py
@@ -103,8 +103,6 @@ class JoystickController():
                     self.show_alarms()
                     # If still pressed, go in edit mode
                     if self.joystick.read_z() == 0:
-                        while self.joystick.read_z() == 0:
-                            time.sleep(0.1)
                         self.edit_alarm()
 
 
@@ -117,10 +115,10 @@ class JoystickController():
         time.sleep(JoystickSettings.press_seconds)
 
 
-    def _process_move(self, value_read, prev_moved):
-        if value_read > JoystickSettings.max_value * 0.7 and prev_moved < 1:
+    def _process_move(self, value_read):
+        if value_read > JoystickSettings.max_value * 0.7:
             return 1
-        elif value_read < JoystickSettings.max_value * 0.3 and prev_moved > -1:
+        elif value_read < JoystickSettings.max_value * 0.3:
             return -1
         else:
             return 0
@@ -133,22 +131,27 @@ class JoystickController():
         editing = 0
         prev_moved_x, prev_moved_y = 0, 0
         self.clock.alarm.display.display_string("Set Alarm", 1)
+        self.clock.alarm.display.display_string(current_alarm, 2)
+        while self.joystick.read_z() == 0:
+            time.sleep(0.1)
 
         while self.joystick.read_z():
-            current_alarm_str = "->" + current_alarm if editing == 0 else current_alarm + "<-"
-            self.clock.alarm.display.display_string(current_alarm_str, 1)
+            current_alarm_str = f"->{current_alarm}  " if editing == 0 else "  {current_alarm}<-"
+            self.clock.alarm.display.display_string(current_alarm_str, 2)
 
             x_read = self.joystick.read_x()
-            moved_x = self._process_move(x_read, prev_moved_x)
-            prev_moved_x = moved_x
-            editing = (editing + moved_x) % 2
+            moved_x = self._process_move(x_read)
+            if prev_moved_x != moved_x:
+                editing = (editing + moved_x) % 2
+                prev_moved_x = moved_x
 
             if moved_x == 0:
                 y_read = self.joystick.read_y()
-                moved_y = self._process_move(y_read, prev_moved_y)
-                prev_moved_y = moved_y
-                current_alarm_ints[editing] += moved_y
-                current_alarm = f"{current_alarm_ints[0]:02d}:{current_alarm_ints[1]:02d}"
+                moved_y = self._process_move(y_read)
+                if prev_moved_y != moved_y:
+                    prev_moved_y = moved_y
+                    current_alarm_ints[editing] += moved_y
+                    current_alarm = f"{current_alarm_ints[0]:02d}:{current_alarm_ints[1]:02d}"
 
 
         json.dump([current_alarm], open(AlarmSettings.alarms_path, "w"))

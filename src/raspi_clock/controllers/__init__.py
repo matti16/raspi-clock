4dbdd484ec5a9d8e12704e2de6bd9cb4ccf6b8c6
matti16 <bmattia92@gmail.com>
2020-12-25 18:44:39 +0100

    move lock

--- src/raspi_clock/controllers/__init__.py
+++ src/raspi_clock/controllers/__init__.py
@@ -99,23 +99,23 @@ class JoystickController():
         self.show_alarms()
         while True:
             # Show alarms on press
-            if self.joystick.read_z() == 0:
-                self.show_alarms()
-                # If still pressed, go in edit mode
-                if self.joystick.read_z() == 0:
-                    while self.joystick.read_z() == 0:
-                        time.sleep(0.1)
-                    self.edit_alarm()
+            if self.joystick.read_z() == 0:  
+                with self.clock.lock:
+                    self.show_alarms()
+                    # If still pressed, go in edit mode
+                    if self.joystick.read_z() == 0:
+                        while self.joystick.read_z() == 0:
+                            time.sleep(0.1)
+                        self.edit_alarm()
 
 
     def show_alarms(self):
-        with self.clock.lock:
-            if len(self.clock.alarm.alarms):
-                self.clock.alarm.display.display_string("Current Alarm", 1)
-                self.clock.alarm.display.display_string(self.clock.alarm.alarms[0], 2)
-            else:
-                self.clock.alarm.display.display_string("No Alarms Set", 1)
-            time.sleep(JoystickSettings.press_seconds)
+        if len(self.clock.alarm.alarms):
+            self.clock.alarm.display.display_string("Current Alarm", 1)
+            self.clock.alarm.display.display_string(self.clock.alarm.alarms[0], 2)
+        else:
+            self.clock.alarm.display.display_string("No Alarms Set", 1)
+        time.sleep(JoystickSettings.press_seconds)
 
 
     def _process_move(self, value_read, prev_moved):
@@ -131,38 +131,37 @@ class JoystickController():
         current_alarm = self.clock.alarm.alarms[0] if len(self.clock.alarm.alarms) else "00:00"
         current_alarm_ints = [int(i) for i in current_alarm.split(":")]
         
-        with self.clock.lock:
-            prev_moved_x, prev_moved_y = 0, 0
-            self.clock.alarm.display.display_string("Set Alarm", 1)
+        prev_moved_x, prev_moved_y = 0, 0
+        self.clock.alarm.display.display_string("Set Alarm", 1)
 
-            while self.joystick.read_z():
+        while self.joystick.read_z():
+            blink = 0
+            editing = 0
+            x_read = self.joystick.read_x()
+
+            if not blink:
+                self.clock.alarm.display.display_string(current_alarm, 2)
+                blink = 1
+            else:
+                current_alarm_show = current_alarm[editing*2 : editing*2+3]
+                self.clock.alarm.display.display_string(current_alarm_show, 2)
                 blink = 0
-                editing = 0
-                x_read = self.joystick.read_x()
 
-                if not blink:
-                    self.clock.alarm.display.display_string(current_alarm, 2)
-                    blink = 1
-                else:
-                    current_alarm_show = current_alarm[editing*2 : editing*2+3]
-                    self.clock.alarm.display.display_string(current_alarm_show, 2)
-                    blink = 0
+            moved_x = self._process_move(x_read, prev_moved_x)
+            prev_moved_x = moved_x
+            editing = (editing + moved_x) % 2
 
-                moved_x = self._process_move(x_read, prev_moved_x)
-                prev_moved_x = moved_x
-                editing = (editing + moved_x) % 2
+            if moved_x == 0:
+                y_read = self.joystick.read_y()
+                moved_y = self._process_move(y_read, prev_moved_y)
+                prev_moved_y = moved_y
+                current_alarm_ints[editing] += moved_y
+                current_alarm = f"{current_alarm_ints[0]:02d}:{current_alarm_ints[1]:02d}"
 
-                if moved_x == 0:
-                    y_read = self.joystick.read_y()
-                    moved_y = self._process_move(y_read, prev_moved_y)
-                    prev_moved_y = moved_y
-                    current_alarm_ints[editing] += moved_y
-                    current_alarm = f"{current_alarm_ints[0]:02d}:{current_alarm_ints[1]:02d}"
+            time.sleep(0.2)
 
-                time.sleep(0.2)
-
-            json.dump([current_alarm], open(AlarmSettings.alarms_path, "w"))
-            self.clock.schedule_alarms()
+        json.dump([current_alarm], open(AlarmSettings.alarms_path, "w"))
+        self.clock.schedule_alarms()
 
 
 

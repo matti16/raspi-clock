b029bbdbbfec8840b1a04d3dcde363b1d4a705b6
matti16 <bmattia92@gmail.com>
2021-02-20 10:41:55 +0100

    test audio script

--- scripts/test_audio.py
+++ scripts/test_audio.py
@@ -1,8 +1,6 @@
 import vlc
 import time
 
-from raspi_clock.setting import AudioSettings
-
 filepath = "../resources/audio/Queen-Dont_Stop_Me_Now.mp3"
 player = vlc.MediaPlayer(filepath)
 player.audio_set_volume(150)
--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-sudo apt install -y python3-pip git supervisor
+sudo apt install -y python3-pip git supervisor vlc
 git clone https://github.com/matti16/raspi-clock.git
 cd raspi-clock
 pip3 install -r requirements.txt
